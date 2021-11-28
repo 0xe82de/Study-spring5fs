8a49593b6a1b192edad4d0f606d27760fb111d3b
0xe82de@gmail.com <0xe82de@gmail.com>
2021-11-28 17:11:24 +0900

    Update: selectByEmail, selectAll 추가

--- sp5-chap08/src/main/java/spring/MemberDao.java
+++ sp5-chap08/src/main/java/spring/MemberDao.java
@@ -1,12 +1,36 @@
 package spring;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class MemberDao {
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	public MemberDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	public Member selectByEmail(String email) {
+		List<Member> results = jdbcTemplate.query(
+				"select * from MEMBER where EMAIL = ?", 
+				(ResultSet rs, int rowNum) -> {
+					Member member = new Member(
+							rs.getString("EMAIL"),
+							rs.getString("PASSWORD"),
+							rs.getString("NAME"),
+							rs.getTimestamp("REGDATE").toLocalDateTime());
+					member.setId(rs.getLong("ID"));
+					return member;
+				},
+				email);
+		return results.isEmpty() ? null : results.get(0);
 	}
 
 	public void insert(Member member) {
@@ -15,6 +39,27 @@ public class MemberDao {
 	public void update(Member member) {
 	}
 
-	public Collection<Member> selectAll() {
+	public List<Member> selectAll() {
+		List<Member> results = jdbcTemplate.query("select * from MEMBER", 
+				new RowMapper<Member>() {
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) 
+					throws SQLException {
+				Member member = new Member(
+						rs.getString("EMAIL"),
+						rs.getString("PASSWORD"),
+						rs.getString("NAME"),
+						rs.getTimestamp("REGDATE").toLocalDateTime());
+				member.setId(rs.getLong("ID"));
+				return member;
+			}
+		});
+		return results;
+	}
+	
+	public int count() {
+		Integer count = jdbcTemplate.queryForObject(
+				"select count(*) from MEMBER", Integer.class);
+		return count;
 	}
 }
