0436147b96e049e03f7c4e37ede76fcb667250c4
김경석 <0xe82de@gmail.com>
2021-11-30 03:53:09 +0900

    Update: @Valid 검증 에러 처리를 ExceptionHandler로 처리하는 코드 추가

--- sp5-chap16/src/main/java/controller/ApiExceptionAdvice.java
+++ sp5-chap16/src/main/java/controller/ApiExceptionAdvice.java
@@ -1,7 +1,10 @@
 package controller;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +18,17 @@ public class ApiExceptionAdvice {
 		return ResponseEntity
 				.status(HttpStatus.NOT_FOUND)
 				.body(new ErrorResponse("no member"));
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleBindException(MethodArgumentNotValidException ex) {
+		String errorCodes = ex.getBindingResult().getAllErrors()
+				.stream()
+				.map(error -> error.getCodes()[0])
+				.collect(Collectors.joining("."));
+		return ResponseEntity
+				.status(HttpStatus.BAD_REQUEST)
+				.body(new ErrorResponse("errorCodes = " + errorCodes));
 	}
 	
 }
