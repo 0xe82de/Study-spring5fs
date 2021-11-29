4e8d65ca70ba617f9353deca5c26458a76253343
김경석 <0xe82de@gmail.com>
2021-11-29 16:20:08 +0900

    Update: GET 요청 매핑 코드 추가
    surveyForm.jsp로 이동

--- sp5-chap11/src/main/java/controller/SurveyController.java
+++ sp5-chap11/src/main/java/controller/SurveyController.java
@@ -1,10 +1,15 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/survey")
 public class SurveyController {
 
+	@GetMapping
+	public String form() {
+		return "survey/surveyForm";
+	}
 }
