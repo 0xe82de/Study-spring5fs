7d17409a855cb3b835a1eadca135d0a0a9fe57aa
김경석 <0xe82de@gmail.com>
2021-11-30 00:13:29 +0900

    Update: 로그인 컨트롤러 빈 등록

--- sp5-chap13/src/main/java/config/ControllerConfig.java
+++ sp5-chap13/src/main/java/config/ControllerConfig.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import controller.LoginController;
 import controller.RegisterController;
 import controller.SurveyController;
+import spring.AuthService;
 import spring.MemberRegisterService;
 
 @Configuration
@@ -13,6 +15,8 @@ public class ControllerConfig {
 
 	@Autowired
 	private MemberRegisterService memberRegSvc;
+	@Autowired
+	private AuthService authService;
 	
 	@Bean
 	public RegisterController registerController() {
@@ -25,4 +29,12 @@ public class ControllerConfig {
 	public SurveyController surveryController() {
 		return new SurveyController();
 	}
+	
+	@Bean
+	public LoginController loginController() {
+		LoginController controller = new LoginController();
+		controller.setAuthService(authService);
+		return controller;
+	}
+	
 }
