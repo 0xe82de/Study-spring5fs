dad157b4cf34847b9165fea82e3f9aa070ee2a41
김경석 <0xe82de@gmail.com>
2021-11-29 22:32:27 +0900

    Create: 회원가입 검증 기능 파일 생성

---
+++ sp5-chap12/src/main/java/controller/RegisterRequestValidator.java
@@ -0,0 +1,20 @@
+package controller;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class RegisterRequestValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
