50d5b87173b6cd3e66acedc9fb0b186691d0376e
김경석 <0xe82de@gmail.com>
2021-11-30 04:24:24 +0900

    Update: 수정자 주석 추가

--- sp5-chap16/src/main/java/spring/Info.java
+++ sp5-chap16/src/main/java/spring/Info.java
@@ -11,7 +11,7 @@ public class Info {
 		System.out.println("version = " + version);
 	}
 	
-//	@Value("${info.version}") // 수정자에도 @Value 애노테이션을 사용할 수 있다.
+//	@Value("${info.version}") // 수정자에도 @Value 애노테이션을 사용할 수 있다
 	public void setVersion(String version) {
 		this.version = version;
 	}
