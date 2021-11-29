c8c060d66f16eb2db969aba29921d58d64f639c2
김경석 <0xe82de@gmail.com>
2021-11-29 15:45:46 +0900

    Update: 회원가입 step3 @ModelAttribute 애노테이션 추가

--- sp5-chap11/src/main/java/controller/RegisterController.java
+++ sp5-chap11/src/main/java/controller/RegisterController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,7 +39,7 @@ public class RegisterController {
 	}
 	
 	@PostMapping("/register/step3")
-	public String handleStep3(RegisterRequest regReq) {
+	public String handleStep3(@ModelAttribute("formData") RegisterRequest regReq) {
 		try {
 			memberRegisterService.regist(regReq);
 			return "register/step3";
