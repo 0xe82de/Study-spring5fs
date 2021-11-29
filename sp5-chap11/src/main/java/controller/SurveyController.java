40abbbbed99dca3c8551a08993ebaf910d02da1f
김경석 <0xe82de@gmail.com>
2021-11-29 16:20:36 +0900

    Update: POST 요청 매핑 추가
    AnswerdData 커맨드 객체를 가지고 submitted.jsp로 이동

--- sp5-chap11/src/main/java/controller/SurveyController.java
+++ sp5-chap11/src/main/java/controller/SurveyController.java
@@ -2,7 +2,11 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import survey.AnsweredData;
 
 @Controller
 @RequestMapping("/survey")
@@ -12,4 +16,10 @@ public class SurveyController {
 	public String form() {
 		return "survey/surveyForm";
 	}
+	
+	@PostMapping
+	public String submit(@ModelAttribute("ansData") AnsweredData data) {
+		return "survey/submitted";
+	}
+	
 }
