44c8577884710b428c1c257c7ac0020990ffa847
김경석 <0xe82de@gmail.com>
2021-11-30 03:39:05 +0900

    Refactor: ResponseEntity로 정상 결과, 비정상 결과 동일한 형식으로 응답 처리

--- sp5-chap16/src/main/java/controller/RestMemberController.java
+++ sp5-chap16/src/main/java/controller/RestMemberController.java
@@ -1,11 +1,14 @@
 package controller;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,27 +39,49 @@ public class RestMemberController {
 		return memberDao.selectAll();
 	}
 	
+//	@GetMapping("/api/members/{id}")
+//	public Member member(@PathVariable Long id,
+//			HttpServletResponse response) throws IOException {
+//		Member member = memberDao.selectById(id);
+//		if (member == null) {
+//			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+//			return null;
+//		}
+//		return member;
+//	}
+//	
+//	@PostMapping("/api/members")
+//	public void newMember(
+//			@RequestBody @Valid RegisterRequest regReq,
+//			HttpServletResponse response) throws IOException {
+//		try {
+//			Long newMemberId = registerService.regist(regReq);
+//			response.setHeader("Location", "/api/members" + newMemberId);
+//			response.setStatus(HttpServletResponse.SC_CREATED);
+//		} catch (DuplicateMemberException dupEx) {
+//			response.sendError(HttpServletResponse.SC_CONFLICT);
+//		}
+//	}
+	
 	@GetMapping("/api/members/{id}")
-	public Member member(@PathVariable Long id,
-			HttpServletResponse response) throws IOException {
+	public ResponseEntity<Object> member(@PathVariable Long id) {
 		Member member = memberDao.selectById(id);
 		if (member == null) {
-			response.sendError(HttpServletResponse.SC_NOT_FOUND);
-			return null;
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ErrorResponse("no member"));
 		}
-		return member;
+		return ResponseEntity.status(HttpStatus.OK).body(member);
 	}
 	
 	@PostMapping("/api/members")
-	public void newMember(
-			@RequestBody @Valid RegisterRequest regReq,
-			HttpServletResponse response) throws IOException {
+	public ResponseEntity<Object> newMember(
+			@RequestBody @Valid RegisterRequest regReq) {
 		try {
 			Long newMemberId = registerService.regist(regReq);
-			response.setHeader("Location", "/api/members" + newMemberId);
-			response.setStatus(HttpServletResponse.SC_CREATED);
+			URI uri = URI.create("/api/members/" + newMemberId);
+			return ResponseEntity.created(uri).build();
 		} catch (DuplicateMemberException dupEx) {
-			response.sendError(HttpServletResponse.SC_CONFLICT);
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 	}
 	
