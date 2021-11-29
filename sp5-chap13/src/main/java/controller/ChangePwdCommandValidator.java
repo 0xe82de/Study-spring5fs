4d4168a907a71a9aa691295f3fc269c7cd14ef7e
김경석 <0xe82de@gmail.com>
2021-11-30 00:34:04 +0900

    Update: 검증 코드 추가

--- sp5-chap13/src/main/java/controller/ChangePwdCommandValidator.java
+++ sp5-chap13/src/main/java/controller/ChangePwdCommandValidator.java
@@ -1,5 +1,20 @@
 package controller;
 
-public class ChangePwdCommandValidator {
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
 
+public class ChangePwdCommandValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return ChangePwdCommand.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "currentPassword", "required");
+		ValidationUtils.rejectIfEmpty(errors, "newPassword", "required");
+	}
+	
 }
