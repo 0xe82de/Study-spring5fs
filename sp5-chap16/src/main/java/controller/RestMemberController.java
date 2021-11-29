327b2160d76ea315f159ef290081254707ed1e32
김경석 <0xe82de@gmail.com>
2021-11-30 03:26:27 +0900

    Update: JSON 데이터를 받아서 회원가입 처리하는 메서드 추가

--- sp5-chap16/src/main/java/controller/RestMemberController.java
+++ sp5-chap16/src/main/java/controller/RestMemberController.java
@@ -4,14 +4,19 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import spring.DuplicateMemberException;
 import spring.Member;
 import spring.MemberDao;
 import spring.MemberRegisterService;
+import spring.RegisterRequest;
 
 @RestController
 public class RestMemberController {
@@ -40,6 +45,19 @@ public class RestMemberController {
 			return null;
 		}
 		return member;
+	}
+	
+	@PostMapping("/api/members")
+	public void newMember(
+			@RequestBody @Valid RegisterRequest regReq,
+			HttpServletResponse response) throws IOException {
+		try {
+			Long newMemberId = registerService.regist(regReq);
+			response.setHeader("Location", "/api/members" + newMemberId);
+			response.setStatus(HttpServletResponse.SC_CREATED);
+		} catch (DuplicateMemberException dupEx) {
+			response.sendError(HttpServletResponse.SC_CONFLICT);
+		}
 	}
 	
 }
