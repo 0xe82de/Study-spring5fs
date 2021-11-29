d5cb18778eb1bd0866dfad6ba75f8ec0db1bb467
김경석 <0xe82de@gmail.com>
2021-11-30 03:32:32 +0900

    Update: 프로퍼티, 생성자, 접근자 추가

--- sp5-chap16/src/main/java/controller/ErrorResponse.java
+++ sp5-chap16/src/main/java/controller/ErrorResponse.java
@@ -2,4 +2,14 @@ package controller;
 
 public class ErrorResponse {
 
+	private String message;
+	
+	public ErrorResponse(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
 }
