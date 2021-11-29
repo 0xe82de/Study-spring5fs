070fc7b13188b2fc35c647d62a5c81b0f5ab1b29
김경석 <0xe82de@gmail.com>
2021-11-30 00:29:13 +0900

    Update: 세션 설정 추가

--- sp5-chap13/src/main/java/controller/LoginController.java
+++ sp5-chap13/src/main/java/controller/LoginController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +28,7 @@ public class LoginController {
 	}
 	
 	@PostMapping
-	public String submit(LoginCommand loginCommand, Errors errors) {
+	public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
 		new LoginCommandValidator().validate(loginCommand, errors);
 		if (errors.hasErrors()) {
 			return "login/loginForm";
@@ -35,6 +37,8 @@ public class LoginController {
 			AuthInfo authInfo = authService.authenticate(
 					loginCommand.getEmail(),
 					loginCommand.getPassword());
+			
+			session.setAttribute("authInfo", authInfo);
 			
 			// TODO 세션에 authinfo 저장해야 함
 			return "login/loginSuccess";
