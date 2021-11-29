cf66f094885286db831e4bcd4f2a3f2936afee11
김경석 <0xe82de@gmail.com>
2021-11-30 03:19:07 +0900

    Update: LocalDateTime 타입 변환 일괄 처리

--- sp5-chap16/src/main/java/config/MvcConfig.java
+++ sp5-chap16/src/main/java/config/MvcConfig.java
@@ -1,10 +1,16 @@
 package config;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.validation.Validator;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,7 +18,10 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import controller.RegisterRequestValidator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import interceptor.AuthCheckInterceptor;
 
 @Configuration
@@ -52,6 +61,18 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Bean
 	public AuthCheckInterceptor authCheckInterceptor() {
 		return new AuthCheckInterceptor();
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // LocalDateTime을 위한 변환 패턴
+		ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
+				.json()
+//				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) // 유닉스 타임 스탬프 출력 기능 비활성화
+//				.simpleDateFormat("yyyyMMddHHmmss") // Date를 위한 변환 패턴
+				.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(formatter)) // LocalDateTime을 위한 변환 패턴
+				.build();
+		converters.add(0, new MappingJackson2HttpMessageConverter(objectMapper));
 	}
 	
 }
