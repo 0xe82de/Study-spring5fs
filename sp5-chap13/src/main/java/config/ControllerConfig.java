180f3af50ebe0cb9ff7c746ba526959993a9cb76
김경석 <0xe82de@gmail.com>
2021-11-30 00:54:36 +0900

    Update: 비밀번호 변경 컨트롤러 빈 등록

--- sp5-chap13/src/main/java/config/ControllerConfig.java
+++ sp5-chap13/src/main/java/config/ControllerConfig.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import controller.ChangePwdController;
 import controller.LoginController;
 import controller.LogoutController;
 import controller.RegisterController;
 import controller.SurveyController;
 import spring.AuthService;
+import spring.ChangePasswordService;
 import spring.MemberRegisterService;
 
 @Configuration
@@ -18,6 +20,8 @@ public class ControllerConfig {
 	private MemberRegisterService memberRegSvc;
 	@Autowired
 	private AuthService authService;
+	@Autowired
+	private ChangePasswordService changePasswordService;
 	
 	@Bean
 	public RegisterController registerController() {
@@ -41,6 +45,13 @@ public class ControllerConfig {
 	@Bean
 	public LogoutController logoutController() {
 		return new LogoutController();
+	}
+	
+	@Bean
+	public ChangePwdController changePwdController() {
+		ChangePwdController controller = new ChangePwdController();
+		controller.setChangePasswordService(changePasswordService);
+		return controller;
 	}
 	
 }
