fb44c3bae512061e011e2b0007c6eba8a566a958
김경석 <0xe82de@gmail.com>
2021-11-29 15:59:34 +0900

    Update: step2 @Model 애노테이션 추가

--- sp5-chap11/src/main/java/controller/RegisterController.java
+++ sp5-chap11/src/main/java/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,10 +27,13 @@ public class RegisterController {
 	}
 	
 	@PostMapping("/register/step2")
-	public String handleStep2(@RequestParam(value="agree", defaultValue="false") Boolean agree) {
+	public String handleStep2(
+			@RequestParam(value="agree", defaultValue="false") Boolean agree,
+			Model model) {
 		if (!agree) {
 			return "register/step1";
 		}
+		model.addAttribute("registerRequest", new RegisterRequest());
 		return "register/step2";
 	}
 	
@@ -39,7 +43,6 @@ public class RegisterController {
 	}
 	
 	@PostMapping("/register/step3")
-	public String handleStep3(@ModelAttribute("formData") RegisterRequest regReq) {
 	public String handleStep3(RegisterRequest regReq) {
 		try {
 			memberRegisterService.regist(regReq);
