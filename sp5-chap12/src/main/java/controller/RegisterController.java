c723080868db59a50d138595843300e16d513757
김경석 <0xe82de@gmail.com>
2021-11-29 23:17:36 +0900

    Update: 컨트롤러 범위 Validator 추가

--- sp5-chap12/src/main/java/controller/RegisterController.java
+++ sp5-chap12/src/main/java/controller/RegisterController.java
@@ -5,7 +5,9 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +58,11 @@ public class RegisterController {
 			errors.rejectValue("email", "duplicate");
 			return "register/step2";
 		}
+	}
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(new RegisterRequestValidator());
 	}
 	
 }
